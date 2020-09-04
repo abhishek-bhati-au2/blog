8149fa25ac091ac92bb2ac8ccd7c882962e8018f
A-bhi <abhisheksinghbhati7@gmail.com>
2020-09-04 09:33:08 +0530

    remove gitignore files

---
+++ server.js
@@ -0,0 +1,33 @@
+const express= require('express')
+const app = express()
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const path = require('path');
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+
+
+
+
+
+mongoose.connect('mongodb://localhost/blog',{ useNewUrlParser: true,useUnifiedTopology: true ,useCreateIndex: true } );
+
+
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended:false }));
+app.use(methodOverride('_method'));
+
+app.get('/', async (req,res)=>{
+    const articles = await Article.find().sort({
+        date: 'desc'
+    });
+    res.render('articles/index', {articles: articles} );
+});
+
+
+
+app.use('/articles',articleRouter);
+
+
+app.listen(8000);
